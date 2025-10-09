c035f1ae7c423490bfb571cdcc26bf991b10980d
Alone_X <62175217+AloneX079@users.noreply.github.com>
2025-10-09 23:08:56 +0530

    HdnSrv

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,8 +1,8 @@
-import axios from 'axios';
+import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: 'https://healthrec.unstablehosting.co.in:25590/api/v1',
-    headers: {
-        'Content-Type': 'application/json',
-    }
+  baseURL: "https://healthrec.unstablehosting.co.in/api/v1",
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
